eb46cb5b47da04ac9dbb0b958a85adf1d3ff7671
mary26pz <66336931+mary26pz@users.noreply.github.com>
2023-06-20 20:26:00 +0000

    CORS fix1

--- html/js/code.js
+++ html/js/code.js
@@ -28,6 +28,7 @@ function doLogin()
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
 	try
 	{
 		xhr.onreadystatechange = function()
